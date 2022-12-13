06e1d72966834bfab407b59d8d2dcb788db6931b
tanypeteryx <tanypteryx@users.noreply.github.com>
2022-12-13 10:39:55 +0100

    Added custom mergesort and solved part 2

--- 2022/Day13/day13.py
+++ 2022/Day13/day13.py
@@ -41,3 +41,48 @@ for ii, pair in enumerate(pairs):
         idx.append(ii+1)
 
 print(sum(idx))
+
+# Part 2
+# make long list
+llist = []
+for pair in pairs:
+    llist += [pair[0],] + [pair[1],]
+llist += [[[2]]] + [[[6]]]
+# build a merge-sort 
+def merge(l, r, key):
+    result = []
+    while l != [] and r != []:
+        if key(l[0],r[0]) == 'T':
+            result.append(l[0])
+            l = l[1:]
+        else:
+            result.append(r[0])
+            r = r[1:]
+    
+    if l != []:
+        result += l
+    if r != []:
+        result += r
+
+    return result
+def merge_sort(mlist, key):
+    if len(mlist) <= 1:
+        return mlist
+    
+    l = []
+    r = []
+    for ii, entry in enumerate(mlist):
+        if ii < len(mlist)//2:
+            l.append(entry)
+        else:
+            r.append(entry)
+    
+    l = merge_sort(l, key)
+    r = merge_sort(r, key)
+
+    return merge(l, r, key)
+
+result = 1
+for ii,line in enumerate(merge_sort(llist, compare_lists)):
+    if line == [[2]] or line == [[6]]: result *= ii+1
+print(result)
