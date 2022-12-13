1ed414f94ddb9915acc83eed208ca3e9c5bd1db2
tanypeteryx <tanypteryx@users.noreply.github.com>
2022-12-13 12:03:58 +0100

    Replaced custom mergsort by cmp_to_key

--- 2022/Day13/day13.py
+++ 2022/Day13/day13.py
@@ -1,58 +1,21 @@
 import json
-
-# Merge Sort
-
-def merge(l, r, key):
-    result = []
-    while l != [] and r != []:
-        if key(l[0],r[0]) == 'T':
-            result.append(l[0])
-            l = l[1:]
-        else:
-            result.append(r[0])
-            r = r[1:]
-    
-    if l != []:
-        result += l
-    if r != []:
-        result += r
-
-    return result
-
-def merge_sort(mlist, key):
-    if len(mlist) <= 1:
-        return mlist
-    
-    l = []
-    r = []
-    for ii, entry in enumerate(mlist):
-        if ii < len(mlist)//2:
-            l.append(entry)
-        else:
-            r.append(entry)
-    
-    l = merge_sort(l, key)
-    r = merge_sort(r, key)
-
-    return merge(l, r, key)
-
-# Comparison logic
+from functools import cmp_to_key
 
 def compare_lists(left, right):
     if isinstance(left, int) and isinstance(right, int):
         if left < right:
-            return "T"
+            return True
         if left > right:
-            return "F"
+            return False
         else:
             return "U"
     if isinstance(left, list) and isinstance(right, list):
         if left == [] and right == []:
             return "U"
         if left != [] and right == []:
-            return "F"
+            return False
         if left == [] and right != []:
-            return "T"
+            return True
         tval = compare_lists(left[0],right[0])
         if tval != 'U':
             return tval
@@ -75,9 +38,8 @@ pairs = [(json.loads(pair[0]), json.loads(pair[1])) for pair in pairs]
 # Part 1
 idx = []
 for ii, pair in enumerate(pairs):
-    if compare_lists(pair[0],pair[1]) == 'T':
+    if compare_lists(pair[0],pair[1]):
         idx.append(ii+1)
-
 print(sum(idx))
 
 # Part 2
@@ -87,8 +49,8 @@ llist = []
 for pair in pairs:
     llist += [pair[0],] + [pair[1],]
 llist += [[[2]]] + [[[6]]]
-
 result = 1
-for ii,line in enumerate(merge_sort(llist, compare_lists)):
+llist.sort(key=cmp_to_key(lambda x, y: -1 if compare_lists(x,y) else 1))
+for ii, line in enumerate(llist):
     if line == [[2]] or line == [[6]]: result *= ii+1
 print(result)
