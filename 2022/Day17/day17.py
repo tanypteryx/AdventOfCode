3243cd89d65a0bf53391338c57551d42cf432044
tanypeteryx <tanypteryx@users.noreply.github.com>
2022-12-18 00:15:33 +0100

    Utter mess after line 87, but it works, somehow...

--- 2022/Day17/day17.py
+++ 2022/Day17/day17.py
@@ -1,4 +1,4 @@
-with open('input.txt') as fid:
+with open('test.txt') as fid:
     data = fid.read().strip()
 
 blows = list(map(lambda x: -1 if x == '<' else 1, data))
@@ -8,10 +8,11 @@ blows = list(map(lambda x: -1 if x == '<' else 1, data))
 # Principal idea: use bitpattern of shapes and the cave to
 # find collisions quickly
 
-# TODO: Major overhaul for Part 2
-# TODO: Bottleneck is the cave list
-# TODO: Replace cave list with number, similar to shapes
-# TODO: NO STRING OPS WHATSOEVER ALLOWED
+# Brute forcing is impossible even with optimization or
+# going to C
+# Discussions indicated repeating patterns starting at
+# some point --> pattern search with manageable number of
+# rocks and sensible pattern lengths
 
 def print_shape(shape):
     if not shape: return None
@@ -22,9 +23,14 @@ def print_shape(shape):
 
 def print_cave(cave):
     if isinstance(cave, list):
-        for line in cave:
+        for ii, line in enumerate(cave):
             str = '{:08b}'.format(line)
-            print(str.replace('0','.'))
+            str = '|'+str[1:]+'|'
+            str = str.replace('1', '#')
+            if ii == len(cave)-1:
+                print('+-------+')
+            else:
+                print(str.replace('0','.'))
     else:    
         print_shape(cave)
     print('\n')
@@ -75,36 +81,66 @@ shapes = [0x1e00, 0x81C0800, 0x4041C00, 0x1010101000, 0x181800]
 # This needs to become a number for Part 2
 cave = [0x80 for _ in range(7)] + [0xFF] # inital
 
+
+### HERE BE DRAGONS
+# Part 2 completely messed up the code
+
 settled = True
-import time
-s = time.time()
 no_of_rocks = 0
 blowpointer = 0
 rockpointer = 0
-while no_of_rocks <= 2022:
+dropdepth = 0
+blowpat = 0
+patterns = []
+heights = []
+while no_of_rocks <= 100000:
     if settled:
+        patterns.append((rockpointer, dropdepth, blowpat))
         rock = shapes[rockpointer]
         rockpointer = (rockpointer +1) % len(shapes)
         cavepointer = 0
+        dropdepth = 0
+        blowpat = 0
         settled = False
         no_of_rocks += 1
-        #if no_of_rocks % 100 == 0:
-        #    cave = prune_cave(cave)
-    # get relevant cave section
+        heights.append(get_height(cave))
     cave_section = get_cave_section(cave, cavepointer)
-    # try to shift
     rock = shift(rock, blows[blowpointer], cave_section)
     blowpointer = (blowpointer + 1) % len(blows)
     if (drop(rock) & cave_section) > 0:
         settled = True
         update_cave(cave, cavepointer, cave_section | rock)
     else:
+        dropdepth += 1
         cavepointer += 1
-print(time.time()-s)
+        if blows[blowpointer] == 1:
+            blowpat += 2**cavepointer
+
 print(get_height(cave))
-    
+patterns = patterns[1:]
+            
+def find_pattern(patterns,lmin,lmax,maxpos):
+    for ii in range(maxpos):
+        for slen in range(lmin,lmax):
+            searcher = patterns[ii:ii+slen]
+            if patterns[ii+slen:ii+2*slen] == searcher:
+                # check the next 10 sequences
+                patcheck = 0
+                for jj in range(2,11):
+                    if patterns[ii+jj*slen:ii+(jj+1)*slen] != searcher:
+                        break
+                    else:
+                        patcheck += 1
+                if patcheck == 9:
+                    print(f'Pattern found! Rock: {ii}, Length: {slen}')
+                    return ii, slen 
 
-  
-
+ppos, plen = find_pattern(patterns,500,5000,50000)
+repeat_height = heights[ppos+plen]-heights[ppos]
+repetitions = (1_000_000_000_000-ppos) // plen
+leftovers = (1_000_000_000_000-ppos) % plen
+leftover_height = heights[ppos+leftovers] - heights[ppos]
+print(heights[ppos]+repeat_height*repetitions)
+print(heights[ppos]+repeat_height*repetitions + leftover_height)
 
 
